50996cacf085058864b95277b908e0bb4df6f73f
Rudraksh <rudraksh.tripathi8@gmail.com>
2022-05-03 21:38:15 +0530

    Invoices app done

--- invoiceapp/admin.py
+++ invoiceapp/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Invoice, Items
 
-admin.site.register(Invoice)
-admin.site.register(Items)
+class ItemsInline(admin.TabularInline):
+    model = Items
+
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ['invoice_id', 'customer_name', 'invoice_date']
+    readonly_fields = ['invoice_date']
+    search_fields = ['invoice_id', 'customer_name', 'invoice_date']
+    inlines = [ItemsInline]
+
+admin.site.register(Invoice, InvoiceAdmin)
+# admin.site.register(Items)
--- invoiceapp/models.py
+++ invoiceapp/models.py
@@ -2,14 +2,18 @@ from django.db import models
 import uuid
 
 class Invoice(models.Model):
-    customer_name = models.CharField(max_length = 200)
-    customer_phone = models.IntegerField(null = True, blank = True)
-    customer_address = models.TextField(null = True, blank = True)
-    invoice_id = models.UUIDField(primary_key = True, unique = True, default=uuid.uuid4)
-    invoice_date = models.DateField(auto_now_add=True)
+    customer_name = models.CharField(max_length = 200, verbose_name='Customer Name')
+    customer_phone = models.IntegerField(null = True, blank = True, verbose_name='Customer Phone')
+    customer_address = models.TextField(null = True, blank = True, verbose_name='Customer Address')
+    invoice_id = models.UUIDField(primary_key = True, unique = True, default=uuid.uuid4, verbose_name='Invoice ID')
+    invoice_date = models.DateField(auto_now_add=True, verbose_name='Invoice Date')
 
     def __str__(self):
         return self.customer_name + ' - ' +str(self.invoice_id)
+    
+    class Meta:
+        verbose_name = 'Invoice'
+        verbose_name_plural = 'Invoices'
 
 class Items(models.Model):
     invoice = models.ForeignKey(Invoice, on_delete = models.CASCADE)
@@ -20,3 +24,7 @@ class Items(models.Model):
 
     def __str__(self):
         return self.item_name
+
+    class Meta:
+        verbose_name = 'Items'
+        verbose_name_plural = 'Items'
