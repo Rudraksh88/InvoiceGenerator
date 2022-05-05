dbc1cc78a5dede676fc907d1280c5bdc01f8e1eb
Rudraksh <rudraksh.tripathi8@gmail.com>
2022-05-05 18:32:58 +0530

    make it better

--- invoiceproject/settings.py
+++ invoiceproject/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from lib2to3.pytree import Base
 from pathlib import Path
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,3 +131,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
